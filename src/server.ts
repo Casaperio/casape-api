2e84e73897581357448f6d29e3bfe8080173f174
CarlosVictor7 <carlos07victor@gmail.com>
2026-01-22 12:33:55 -0300

    fix: Add 'x-api-key' to allowed headers in CORS configuration

--- src/server.ts
+++ src/server.ts
@@ -35,7 +35,7 @@ export async function createServer() {
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
   });
 
   // Register all routes
